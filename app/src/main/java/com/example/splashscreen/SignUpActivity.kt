0f76406485d6470f0daaf8f028aba9d8d0eb3699
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-06-09 17:56:14 +0300

    password and email confirmation

--- app/src/main/java/com/example/splashscreen/SignUpActivity.kt
+++ app/src/main/java/com/example/splashscreen/SignUpActivity.kt
@@ -1,11 +1,91 @@
 package com.example.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var btnLoginTwo:Button
+    lateinit var tilFirstName:TextInputLayout
+    lateinit var tilLastName:TextInputLayout
+    lateinit var btnSignUp:Button
+    lateinit var tilPasswordTwo:TextInputLayout
+    lateinit var tilEmailTwo:TextInputLayout
+    lateinit var tilConfirm:TextInputLayout
+    lateinit var etFirstName:EditText
+    lateinit var etLastName:EditText
+    lateinit var etConfirm:EditText
+    lateinit var etPasswordTwo:EditText
+    lateinit var etEmailTwo:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        btnLoginTwo=findViewById(R.id.btnLoginTwo)
+        tilFirstName=findViewById(R.id.tilFirstName)
+        tilLastName=findViewById(R.id.tilLastName)
+        btnSignUp=findViewById(R.id.btnSignUp)
+        tilConfirm=findViewById(R.id.tilConfirm)
+        tilEmailTwo=findViewById(R.id.tilEmailTwo)
+        tilPasswordTwo=findViewById(R.id.tilPasswordTwo)
+        etConfirm=findViewById(R.id.etConfirm)
+        etFirstName=findViewById(R.id.etFirstname)
+        etLastName=findViewById(R.id.etLastName)
+        etEmailTwo=findViewById(R.id.etEmailTwo)
+        etPasswordTwo=findViewById(R.id.etPasswordTwo)
+        btnSignUp.setOnClickListener { validateSignup() }
+        btnLoginTwo.setOnClickListener {
+            val intent= Intent(this,loginActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
+    fun validateSignup(){
+        var error=false
+        tilFirstName.error=null
+        tilLastName.error=null
+        tilEmailTwo.error=null
+        tilPasswordTwo.error=null
+        tilConfirm.error=null
+        var email=etEmailTwo.text.toString()
+        if (email.isBlank()){
+            tilEmailTwo.error="Email is required"
+            error=true
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            tilEmailTwo.error="Not a valid email address"
+            error=true
+        }
+        var password=etPasswordTwo.text.toString()
+        if (password.isBlank()){
+            tilPasswordTwo.error="Password is required"
+            error=true
+        }
+        var firstName=etFirstName.text.toString()
+        if (firstName.isBlank()) {
+            tilFirstName.error = "FirstName is required"
+            error=true
+        }
+        var lastName=etLastName.text.toString()
+        if (lastName.isBlank()) {
+            tilLastName.error = "lastName is required"
+            error=true
+        }
+        var Confirm=etConfirm.text.toString()
+        if (Confirm.isBlank()) {
+            tilConfirm.error = "Confirmation is required"
+        }
+        if (!error){
+
+        }
+        if (password!=Confirm){
+            tilConfirm.error="Invalid confirmation"
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/splashscreen/loginActivity.kt
+++ app/src/main/java/com/example/splashscreen/loginActivity.kt
@@ -1,35 +1,41 @@
 package com.example.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class loginActivity : AppCompatActivity() {
-    lateinit var btnLogin:Button
-    lateinit var tilEmail:TextInputLayout
-    lateinit var etEmail:TextInputEditText
-    lateinit var tilPassword:TextInputLayout
-    lateinit var etPassword:TextInputEditText
-
+    lateinit var btnLogin: Button
+    lateinit var tilEmail: TextInputLayout
+    lateinit var etEmail: TextInputEditText
+    lateinit var tilPassword: TextInputLayout
+    lateinit var etPassword: TextInputEditText
+    lateinit var tvSignUp: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        castViews()
-    }
-    fun castViews(){
-        btnLogin=findViewById(R.id.btnLogin)
-        tilEmail=findViewById(R.id.tilEmail)
-        etEmail=findViewById(R.id.etEmail)
-        tilPassword=findViewById(R.id.tilPassword)
-        etPassword=findViewById(R.id.etPassword)
-        btnLogin.setOnClickListener{validateLogin()}
+        tvSignUp = findViewById(R.id.tvSignUp)
+        btnLogin = findViewById(R.id.btnLogin)
+        tilEmail = findViewById(R.id.tilEmailTwo)
+        etEmail = findViewById(R.id.etEmail)
+        tilPassword = findViewById(R.id.tilPasswordTwo)
+        etPassword = findViewById(R.id.etPasswordTwo)
+        btnLogin.setOnClickListener { validateLogin() }
+        tvSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
     fun validateLogin(){
         var error=false
         tilPassword.error=null
         tilEmail.error=null
+
         var email=etEmail.text.toString()
         if (email.isBlank()){
             tilEmail.error="Email is required"
@@ -38,8 +44,14 @@ class loginActivity : AppCompatActivity() {
         var password=etPassword.text.toString()
         if (password.isBlank()){
             tilPassword.error="Password is required"
+            error=true
         }
+        if (!error){
 
         }
-
     }
+}
+
+
+
+
